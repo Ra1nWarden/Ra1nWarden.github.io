035e1f56da5298da226afe3f4594b1e9eab858e5
Ra1nWarden <wzh19921016@gmail.com>
2014-03-19 19:57:09 -0400

    Fixes issue

--- howhardcanyoumug/js/html_actuator.js
+++ howhardcanyoumug/js/html_actuator.js
@@ -79,7 +79,7 @@ HTMLActuator.prototype.addTile = function (tile) {
       inner.textContent = "政力";
       break;
       case 64:
-      inner.textContnet = "玮姐";
+      inner.textContent = "玮姐";
       break;
       case 128:
       inner.textContent = "王砾";
