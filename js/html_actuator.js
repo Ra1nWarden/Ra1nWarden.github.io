9a1982d2a919b9d9735c5a58d534f48a8ef117dd
Ra1nWarden <wzh19921016@gmail.com>
2014-03-19 19:27:42 -0400

    Done!

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -62,7 +62,41 @@ HTMLActuator.prototype.addTile = function (tile) {
   this.applyClasses(wrapper, classes);
 
   inner.classList.add("tile-inner");
-  inner.textContent = tile.value;
+  switch (tile.value) {
+      case 2: 
+      inner.textContent = "子豪";
+      break;
+      case 4:
+      inner.textContent = "健侨";
+      break;
+      case 8:
+      inner.textContent = "郭天";
+      break;
+      case 16:
+      inner.textContent = "张钊";
+      break;
+      case 32:
+      inner.textContent = "政力";
+      break;
+      case 64:
+      inner.textContnet = "玮姐";
+      break;
+      case 128:
+      inner.textContent = "王砾";
+      break;
+      case 256:
+      inner.textContent = "逸嘉";
+      break;
+      case 512:
+      inner.textContent = "傲晖";
+      break;
+      case 1024:
+      inner.textContent = "华神";
+      break;
+      case 2048:
+      inner.textContent = "兴哥";
+      break;
+  }
 
   if (tile.previousPosition) {
     // Make sure that the tile gets rendered in the previous position first
@@ -126,7 +160,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "Game over!";
+  var message = won ? "You win! Xingge reborn!" : "Mug harder!";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
